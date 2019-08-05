866c92b91344d1d01223cfd2c32bc946b2fe418d
BenR <20133902+thisbenrogers@users.noreply.github.com>
2019-08-05 15:22:48 -0500

    Adds Basic Structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import TodoForm from "./components/TodoComponents/TodoForm";
+import TodoList from "./components/TodoComponents/TodoList";
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -8,6 +11,8 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList />
+        <TodoForm />
       </div>
     );
   }
